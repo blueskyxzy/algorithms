f1cdcfaee2a85d132dfae6bcc0d75be1c3b4f490
blueskyxzy <blueskyxzy@gmail.com>
2018-05-04 14:00:20 +0800

    直接选择排序

--- selectsortxzy.py
+++ selectsortxzy.py
@@ -2,15 +2,27 @@
 # -*- coding: UTF-8 -*-
 from datetime import datetime
 import testdata
+# 直接选择排序
+# 遍历数组，依次找到最小的放在前面
+# i从1到len，i到len找到最小的缩影，与i交换位置
 
 
 def selectsortxzy(x):
-
-
+    if x == None or len(x) < 2:
+        return x
+    for i in range(len(x)):
+        index = i
+        for j in range(i, len(x)):
+            if x[j] < x[index]:
+                index = j
+        tmp = x[i]
+        x[i] = x[index]
+        x[index] = tmp
+    return x
 
 
 if __name__ == '__main__':
-    arr = testdata.arr1
+    arr = testdata.arr2
     time1 = datetime.now()
     selectsortxzy(arr)
     time2 = datetime.now()
